cd71565dac0de4111e25446b9298fcaa86539fd9
Chao Ning <chao.ning.uk@gmail.com>
2024-02-14 22:48:11 +0000

    add router for sending pandas data in POST

--- src/my_fastapi/main.py
+++ src/my_fastapi/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 
 from my_fastapi.utils.error_handling import setup_error_handling
 
-from .routers import root
+from .routers import pd_data, root
 
 app = FastAPI()
 
 app.include_router(root.router)
+app.include_router(pd_data.router)
 
 setup_error_handling(app)
 
---
+++ src/my_fastapi/test_main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_hello():
+    response = client.get("/hello/Loong")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello Loong"}
+
+
+def test_settings():
+    response = client.get("/settings")
+    assert response.status_code == 200
+    assert response.json() == {"env": "local"}
+
+
+def test_csv_df():
+    df = pd.DataFrame(data={"a": [1, 2, 3], "b": [2, 3, 4], "c": [5, 6, 7]})
+    response = client.post(
+        "/pd_data/csv/abc.csv",
+        # headers={'Content-type': 'application/octet-stream'},
+        headers={"Content-type": "text/csv"},
+        content=df.to_csv(),
+        params={"index_col": "a", "y_column": "b", "x_columns": ["c"]},
+    )
+    # assert response.status_code == 200
+    assert response.json() == {
+        "filename": "abc.csv",
+        "y_column": "b",
+        "x_columns": ["c"],
+        "df.index": "a",
+        "len(df)": 3,
+    }
