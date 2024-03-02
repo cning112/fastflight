b7ea710b6d146c4d675828e4ab68ab97018fd090
Chao Ning <chao.ning.uk@gmail.com>
2024-03-02 01:07:45 +0000

    use vite to create a react+ts project

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,39 +1,47 @@
 // @ts-check
 
 import eslint from "@eslint/js";
-import jestPlugin from "eslint-plugin-jest";
-import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
+import reactPlugin from "eslint-plugin-react";
+import reactRefreshPlugin from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import storybookPlugin from "eslint-plugin-storybook";
 
 export default tseslint.config(
-	{
-		ignores: ["**/build/**", "**/dist/**"],
-	},
-	eslint.configs.recommended,
-	...tseslint.configs.recommendedTypeChecked,
-	...tseslint.configs.stylisticTypeChecked,
-	{
-		plugins: {
-			react: reactPlugin,
-			"react-hooks": reactHooksPlugin,
-			storybook: storybookPlugin,
-		},
-		languageOptions: {
-			parserOptions: {
-				project: true,
-				tsconfigRootDir: import.meta.dirname,
-			},
-		},
-	},
-	{
-		files: ["*.js"],
-		...tseslint.configs.disableTypeChecked,
-	},
-	{
-		// enable jest rules on test files
-		files: ["test/**"],
-		...jestPlugin.configs["flat/recommended"],
-	},
+  {
+    ignores: ["**/build/**", "**/dist/**"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-refresh": reactRefreshPlugin,
+    },
+    languageOptions: {
+      /**
+       * Doing so indicates that each source file being linted should use type information based on the
+       * nearest tsconfig.json in its directory. For each file, @typescript-eslint/parser will check
+       * that file's directory, then the parent directory, and so on - until a tsconfig.json file is found.
+       */
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    files: ["*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  }
 );
